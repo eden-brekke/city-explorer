8d3cc9133d65963b2f2251124f44bcb9710e8112
Eden Brekke <eden.lorrai@gmail.com>
2022-03-21 20:23:13 -0700

    done

--- src/CityDisplay.js
+++ src/CityDisplay.js
@@ -8,9 +8,9 @@ class CityDisplay extends React.Component {
       <div className="card-Div">
         <Card className="h-100">
           <Card.Body>
-            <Card.Title>{this.props.cityData.display_name}</Card.Title>
-            <Card.Text>{this.props.cityData.lon}</Card.Text>
-            <Card.Text>{this.props.cityData.lat}</Card.Text>
+            <Card.Title>City:  {this.props.cityData.display_name}</Card.Title>
+            <Card.Text>Longitude: {this.props.cityData.lon}</Card.Text>
+            <Card.Text>Latitude: {this.props.cityData.lat}</Card.Text>
           </Card.Body>
         </Card>
       </div>
