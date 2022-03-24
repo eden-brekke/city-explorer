f96d3e7ea53f83da83cc26b19c44be5a67dd4b3f
Eden Brekke <eden.lorrai@gmail.com>
2022-03-23 17:33:15 -0700

    by golly I got it

---
+++ src/components/movies/Movie.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import Moviedata from './Moviedata';
+import './Movie.css'
+
+class Movie extends React.Component {
+
+  render() {
+    let movieResults = this.props.movieData.map((movie, index) => {
+      console.log(index)
+      console.log(movie)
+      return (
+       <Moviedata
+       key = {index}  
+       movie={movie}
+       />
+      );
+    }
+    );
+    return (
+      <>
+        <Modal
+          show={this.props.movieModal}
+          onClick={this.props.hideMovieModal}
+        >
+          <Modal.Header className="modal-head" closeButton>Movies with your Searched City in the Title</Modal.Header>
+          <Modal.Body className="modal-body" onClick={this.props.hideMovieModal}>
+            {movieResults}
+          </Modal.Body>
+        </Modal> 
+      </>
+    )
+  }
+}
+    export default Movie;
